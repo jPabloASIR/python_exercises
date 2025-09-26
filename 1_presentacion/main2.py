a9d365d1c565a2cad0132b153886e58337a3c7cf
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-09-26 13:05:06 +0200

    Add files via upload

---
+++ 1_presentacion/main2.py
@@ -0,0 +1,6 @@
+nombre = input("¿Cómo te llamas? ")
+edad = input ("¿Cuál es tu edad?")
+altura = input ("¿Cuánto mides?")
+estudiante = True
+
+print(f"Me llamo {nombre}! tengo {edad} años, mido {altura} cm y soy estudiante: {estudiante}.")
