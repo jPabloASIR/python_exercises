505a13190e96df21af7182143b5feb84784146b6
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-09-26 13:28:59 +0200

    Add files via upload

---
+++ 1_presentacion/b1_3_conversor_datos.py
@@ -0,0 +1,8 @@
+nombre = input("¿Cómo te llamas? ")
+edad = input ("¿Cuál es tu edad? ")
+altura = input ("¿Cuánto mides? ")
+
+int(edad)
+float(altura)
+
+print(f"Me llamo {nombre}! tengo {edad} años y mido {altura} cm.")
