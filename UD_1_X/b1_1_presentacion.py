8051e2f33ad101f693e1d7ca8ad94ebfbb955645
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-09-28 18:29:13 +0200

    Update b1_1_presentacion.py

--- UD_1_X/b1_1_presentacion.py
+++ UD_1_X/b1_1_presentacion.py
@@ -1,7 +1,8 @@
 nombre = input("¿Cómo te llamas? ")
-edad = input ("¿Cuál es tu edad?")
-altura = input ("¿Cuánto mides?")
+edad = input ("¿Cuál es tu edad? ")
+altura = input ("¿Cuánto mides? ")
 estudiante = input("¿Eres estudiante? (Si/No) ") == "Si"
 
 
 print(f"Me llamo {nombre}! tengo {edad} años, mido {altura} cm y soy estudiante: {estudiante}.")
+
