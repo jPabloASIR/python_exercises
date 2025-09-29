ea9df3919e94a1e70b3e5f66d768ca2b5b012fe2
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-09-29 12:13:56 +0200

    Update b1_4_login.py

--- UD_1_X/b1_4_login.py
+++ UD_1_X/b1_4_login.py
@@ -2,10 +2,11 @@ usuario_correcto = "admin"
 contrasena_correcta = "1234"
 
 usuario = input("¿Cuál es tu usuario? ")
-contra = input("¿Cuál es su contraseña ")
+contra = input("¿Cuál es su contraseña? ")
 
 if (usuario_correcto == usuario and contrasena_correcta == contra):
     print("Acceso concedido.")
 
 else:
+
     print("No has aprobado, womp womp.")
