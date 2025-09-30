b8cc297f112eef43781b87712e5d5cb3a4c14d59
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-09-30 08:22:17 +0200

    Update b1_5_calculadora_condicional.py

--- UD_1_X/b1_5_calculadora_condicional.py
+++ UD_1_X/b1_5_calculadora_condicional.py
@@ -1,24 +1,24 @@
-num1 = input("Elige un número ")
-num2 = input("Elige un segundo número ")
+num1 = float(input("Elige un número "))
+num2 = float(input("Elige un segundo número "))
 
-print( "1.Suma"
-        "2.Resta"
-        "3.Multiplicación"
-        "4.División")
+x = int(input( "1.Suma"
+               "2.Resta"
+               "3.Multiplicación"
+               "4.División"
+               ":"))
 
-x = input("Su respuesta: ")
 
-if int(x) == 1:
-    x = float(num1) + float(num2)
-    print(f"La suma de {num1} y {num2} es: {x}")
-elif int(x) == 2:
-    x = float(num1) - float(num2)
-    print(f"La suma de {num1} y {num2} es: {x}")
-elif int(x) == 3:
-    x = float(num1) * float(num2)
-    print(f"La suma de {num1} y {num2} es: {x}")
-elif int(x) == 4:
-    x = float(num1) / float(num2)
-    print(f"La suma de {num1} y {num2} es: {x}")
+if x == 1:
+    print(f"La suma de {num1} y {num2} es: {num1 + num2}")
+    
+elif x == 2:
+    print(f"La resta de {num1} y {num2} es: {num1 - num2}")
+
+elif x == 3:
+    print(f"La multiplicación de {num1} y {num2} es: {num1 * num2}")
+    
+elif x == 4:
+    print(f"La división de {num1} y {num2} es: {num1 / num2}")
+    
 else:
     print("Operación no reconocida")
