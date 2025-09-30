3d93ebb495d5204119db3dc575bab3636e5a1a2a
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-09-30 10:04:54 +0200

    Add files via upload

---
+++ UD_2_X/b2_10_2_enumerate_avanzado.py
@@ -0,0 +1,29 @@
+estudiantes = ["Ana", "Luis", "Marta", "Carlos"]
+notas_matematicas = [8, 7, 9, 6]
+notas_fisica = [9, 6, 10, 7]
+notas_quimica = [7, 8, 9, 5]
+'''
+
+res = [float(sum(notas_matematicas + notas_fisica + notas_quimica) / len(notas_matematicas + notas_fisica + notas_quimica))]
+
+'''
+
+def media(numeros):
+    return float(sum(numeros)) / max(len(numeros), 1)
+
+
+avg = [media(i) for i in zip(notas_matematicas,notas_fisica, notas_quimica)]
+
+
+state = []
+for promedio in avg:
+    if promedio >= 6.5:
+        state.append("Aprobado")
+    elif promedio >= 5:
+        state.append("En recuperación")
+    else:
+        state.append("Reprobado")
+
+
+for indice, (a, b, c, d, e, f) in enumerate(zip(estudiantes, notas_matematicas, notas_fisica, notas_quimica, avg, state), start=1):
+    print(indice, a, "- Matemáticas: ", b, " Física: ", c, " Química: ", d, " Promedio: ", e, " Estado: ", f)
