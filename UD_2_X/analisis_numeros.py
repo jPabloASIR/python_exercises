b8290ade300425d427970b49d1f36b43c2116cad
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-10-06 14:21:43 +0200

    Update analisis_numeros.py

--- UD_2_X/analisis_numeros.py
+++ UD_2_X/analisis_numeros.py
@@ -9,7 +9,7 @@ for n in range(0, 20):
     print(lista)
     cuadrado.append(lista)
     par.append(lista)
-    idk.append(lista % 2 == 0)
+    idk.append(lista)
 
 print("\nMostrando los cuadrados de los números de la lista....")
 
@@ -21,4 +21,5 @@ par = [n % 2 == 0 for n in par]
 print(par)
 
 print("\nMostrando solo los mayores de 10 de la lista....")
+idk = [n > 10 for n in idk]
 print(idk)
