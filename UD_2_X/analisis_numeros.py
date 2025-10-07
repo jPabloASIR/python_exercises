420f59278c4d56491302cd883a07bc5735b5196c
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-10-07 22:23:49 +0200

    Update analisis_numeros.py

--- UD_2_X/analisis_numeros.py
+++ UD_2_X/analisis_numeros.py
@@ -3,13 +3,16 @@ lista = []
 cuadrado = []
 par = []
 idk = []
+dobles = {}
 
 for n in range(0, 20):
-    lista = random.randint(0,20)
+    num = random.randint(0, 20) ''' num contiene el valor actual del número aleatorio'''
+    lista.append(num) ''' Ese valor se guarda en la posición correspondiente de la lista llamada lista '''
     print(lista)
     cuadrado.append(lista)
     par.append(lista)
     idk.append(lista)
+    dobles[num] = num * 2 ''' Doble del valor actual de num '''
 
 print("\nMostrando los cuadrados de los números de la lista....")
 
@@ -23,3 +26,6 @@ print(par)
 print("\nMostrando solo los mayores de 10 de la lista....")
 idk = [n > 10 for n in idk]
 print(idk)
+
+print("\nDiccionario con los números y sus dobles:")
+print(dobles)
