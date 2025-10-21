5cf6962ab5c6c514bde310de0ab0da113ca32265
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-10-21 08:13:39 +0200

    Update b1_7_filtrar_lista.py

--- UD_1_X/b1_7_filtrar_lista.py
+++ UD_1_X/b1_7_filtrar_lista.py
@@ -1,10 +1,11 @@
 lista = ["Ana", "Pedro", "Alba", "Lucía", "albA", "alejAndrA"]
-''' Este for recorre toda la variable lista desde la posición 0 (Ana) hasta la posición 5 (alejAndrA) y observa si la primera letra  del contenido de cada posición empieza por A, si no es así, aparecerá por pantalla guardada en la variable i.'''
+""" Este for recorre toda la variable lista desde la posición 0 (Ana) hasta la posición 5 (alejAndrA) y observa si la primera letra  del contenido de cada posición empieza por A, si no es así, aparecerá por pantalla guardada en la variable i. """
 for i in lista:
     if i[0] in 'A':
 
         continue
     print(f"Contraseña válida: {i}")
+
 
 
 
