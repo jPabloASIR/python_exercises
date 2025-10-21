9e60eba5a93a6f81c6c7553d8821ff4e79b9cb43
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-10-21 08:15:13 +0200

    Refactor comments in multiplication table script
    Updated comments for clarity and improved explanation.

--- UD_1_X/b1_8_tabla_multiplicar.py
+++ UD_1_X/b1_8_tabla_multiplicar.py
@@ -1,7 +1,8 @@
-'''Script que recoge 1 número pasado por teclado guardada en la variable num con bucle for '''
+""" Script que recoge 1 número pasado por teclado guardada en la variable num con bucle for """
 
 num = int(input("Escriba su número: "))
-'''Como el bucle for debe de ir desde el 0 y terminar en el 10, su rango es de 0 a 11'''
+"""Como el bucle for debe de ir desde el 0 y terminar en el 10, su rango es de 0 a 11, porque en range() el final no se incluye y en nuestro caso nuestro final es 11 """
 for i in range(0, 11):
     print(num, 'x', i, '=', num*i)
+
 
