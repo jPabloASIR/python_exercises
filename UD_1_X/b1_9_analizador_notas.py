c4ee9a3776c82659d436948fe73a190faf8f58da
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-10-21 08:15:46 +0200

    Update b1_9_analizador_notas.py

--- UD_1_X/b1_9_analizador_notas.py
+++ UD_1_X/b1_9_analizador_notas.py
@@ -1,4 +1,4 @@
-'''El ejercicio recoge 3 números por teclado y procede a guardar su suma y más tarde división en la variable promedio.
+"""El ejercicio recoge 3 números por teclado y procede a guardar su suma y más tarde división en la variable promedio.
 
 
 El problema residía en la antigua línea 19:
@@ -9,7 +9,7 @@ Daba como resultado promedio = 7.0 cuando nota1 = nota2 = nota3 = 3 lo cual es o
 Esto se debe a que primero realiza la operación nota3 / 3 y luego las sumas.
 
 Se ha solucionado agregándole paréntesis para que tenga mayor prioridad la suma, como se ve en el código actual.
-'''
+"""
 
 
 nota1 = float(input("Introduzca la primera nota: "))
@@ -17,5 +17,6 @@ nota2 = float(input("Introduzca la segunda nota: "))
 nota3 = float(input("Introduzca la tercera nota: "))
 
 promedio = (nota1 + nota2 + nota3) / 3
+
 
 print(f"El promedio de las notas {nota1}, {nota2} y {nota3} es igual a: {promedio}")
