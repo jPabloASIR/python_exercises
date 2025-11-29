196661ad1c163aee8bc8a32c172b0f55459aee36
jPabloASIR <pablogutierrezjerez@gmail.com>
2025-11-29 12:48:01 +0100

    Update informacion_persona.py

--- UD_2_X/informacion_persona.py
+++ UD_2_X/informacion_persona.py
@@ -1,6 +1,12 @@
+"""
+Tenemos una tupla cuyas variables están desempaquetadas en nombre, edad y ciudad.
+Imprimimos en orden las 3 tal y como nos lo pide el ejercicio.
+"""
+
 persona = ("Ana", 25, "Madrid")
 nombre, edad, ciudad = persona
 
-print(f"Nombre: {nombre}")  # Ana
-print(f"Edad: {edad}")    # 25
-print(f"Ciudad: {ciudad}")  # Madrid
+print(f"Nombre: {nombre}")  
+print(f"Edad: {edad}")    
+
+print(f"Ciudad: {ciudad}")  
